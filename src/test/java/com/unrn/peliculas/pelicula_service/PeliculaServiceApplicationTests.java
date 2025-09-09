a092f5e3eeb81b7be07d86f15cc2418f67f5bba4
marce <mchamorro@fiveit.com.ar>
2025-09-08 22:14:17 -0300

    initial commit

---
+++ src/test/java/com/unrn/peliculas/pelicula_service/PeliculaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unrn.peliculas.pelicula_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
